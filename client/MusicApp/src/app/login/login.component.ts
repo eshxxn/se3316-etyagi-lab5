9eb00ee5548f995fbd0c3c8dab893fbb931bbbf1
eshxxn <etyagi@uwo.ca>
2019-12-03 23:31:43 -0500

    Commit 16
    Input sanitation added

--- client/MusicApp/src/app/login/login.component.ts
+++ client/MusicApp/src/app/login/login.component.ts
@@ -14,7 +14,15 @@ export class LoginComponent implements OnInit {
   login(){
     var email = (<HTMLInputElement>document.getElementById("uname")).value;
     var password = (<HTMLInputElement>document.getElementById("psw")).value;
-
+    //input sanitation
+        if (email!== "undefined") {
+              let lastAtPos = email.lastIndexOf('@')
+              let lastDotPos = email.lastIndexOf('.');
+              if (!(lastAtPos < lastDotPos && lastAtPos > 0 && email.indexOf('@@') === -1 && lastDotPos > 2 && (email.length - lastDotPos) > 2)) {
+                  document.getElementById("error").innerHTML = "Username contains illegal characters";
+                  return false;
+              }
+            }
     const http = new XMLHttpRequest;
     const url = "http://localhost:1234/user/login";
     http.open("POST", url);
--- client/MusicApp/src/app/register/register.component.ts
+++ client/MusicApp/src/app/register/register.component.ts
@@ -14,16 +14,29 @@ export class RegisterComponent implements OnInit {
 
   register(){
     console.log("Worked");
-    var email = (<HTMLInputElement>document.getElementById("uname")).value;
-    var password = (<HTMLInputElement>document.getElementById("psw")).value;
+    var email = (<HTMLInputElement>document.getElementById("username")).value;
+    var password = (<HTMLInputElement>document.getElementById("pass")).value;
 
+//input sanitation
+    if (email!== "undefined") {
+          let lastAtPos = email.lastIndexOf('@')
+          let lastDotPos = email.lastIndexOf('.');
+          if (!(lastAtPos < lastDotPos && lastAtPos > 0 && email.indexOf('@@') === -1 && lastDotPos > 2 && (email.length - lastDotPos) > 2)) {
+              document.getElementById("error").innerHTML = "Username contains illegal characters";
+              return false;
+          }
+        }
+        else{
+          document.getElementById("error").innerHTML = "Invalid username!";
+          return false;
+        }
     const http = new XMLHttpRequest;
     const url = "http://localhost:1234/user/register";
     http.open("POST", url);
     let body = new URLSearchParams();
 
-    body.append("uname", email);
-    body.append("psw", password);
+    body.append("username", email);
+    body.append("password", password);
     http.send(body);
 
 
