567a63160e550abb0fd86454c73ccc17c2480bc0
eshxxn <etyagi@uwo.ca>
2019-12-03 21:40:18 -0500

    Commit 11
    Attempted to fix the register page

--- client/MusicApp/src/app/register/register.component.ts
+++ client/MusicApp/src/app/register/register.component.ts
@@ -12,4 +12,27 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  register(){
+    console.log("Worked");
+    var email = (<HTMLInputElement>document.getElementById("uname")).value;
+    var password = (<HTMLInputElement>document.getElementById("psw")).value;
+
+    const http = new XMLHttpRequest;
+    const url = "http://localhost:1234/user/register";
+    http.open("POST", url);
+    let body = new URLSearchParams();
+
+    body.append("uname", email);
+    body.append("psw", password);
+    http.send(body);
+
+
+  //  http.onreadystatechange = () =>{
+    //  if(http.status == 200){
+        //this.newService.active = true;
+      //  window.location.href = "http://localhost:4200/";
+        //document.getElementById("error").innerHTML = "User was signed in";
+        //document.getElementById("error").style.color = "green";
+//    }
+}
 }
